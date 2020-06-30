dc785fd50693f0db31fae1c25d1aaa439c9a905d
malyarc <jmtang1526@gmail.com>
2020-06-30 05:20:05 -0700

    Created step 1 of application. Created API req and OAuth2 for Amazon/facebook/github/google strategies

---
+++ client/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route } from "react-router-dom";
+import history from "./history.js";
+import UserProvider from "./contexts/UserProvider.jsx";
+import Home from "./pages/Home.jsx";
+import Profile from "./pages/Profile.jsx";
+import MenuBar from "./components/menus/MenuBar.jsx";
+
+
+const App = () => {
+
+    return (
+
+        <Router history={history}>
+            <UserProvider>
+                <Route path='/' component={MenuBar} />
+                <Route path='/profile' component={Profile} />
+            </UserProvider>
+            <Route path='/' exact component={Home} />
+        </Router>
+
+
+        )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  watch: true,
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  mode: "development",
+  entry: `${SRC_DIR}/app.jsx`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js',
+
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+  },
+};
